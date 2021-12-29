07de8fd084e55ce8fa8430d4ca5ae668c99b565b
Elmano Neves Junior <49047777+padrerolin@users.noreply.github.com>
2021-12-29 15:30:26 -0300

    Add files via upload
    Primeiros Codigos CSharp

---
+++ Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Enum
+{
+    enum Pessoas
+    {
+        Giovanna,
+        Julia,
+        João,
+        Gustavo,
+        Ronaldo
+    }
+}
+
+
---
+++ Interface.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Interface
+{
+
+    interface IAnimal
+    {
+        void Nome(string nome);
+        void Dono(string nomeDono);
+        void Especie(string especie);
+
+        
+    }
+
+    class Animal : IAnimal
+    {
+         public string Nome { get; set; }
+         public string nomeDono { get; set; }
+         public string Especie  { get; set; }
+
+        void IAnimal.Nome(string nome)
+        {
+            this.Nome = nome;
+        }
+        void IAnimal.Dono(string nomeDono)
+        {
+            this.nomeDono = nomeDono;
+        }
+        void IAnimal.Especie(string Especie)
+        {
+            this.Especie = Especie;
+        }
+
+    }
+
+
+}
